4bdf7a4422c5460587469f161e2080020bd8f510
Ovikx <ovikdas10@gmail.com>
2023-07-13 00:24:52 -0400

    first commit

---
+++ src/Thing.tsx
@@ -0,0 +1,5 @@
+export function Thing() {
+    return (
+        <p className='underline text-white'>Hello world!</p>
+    );
+}
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+import { Thing } from './Thing';
+
+export function App() {
+  return (
+    <>
+      <div className="flex-1 bg-gray-900">
+        <Thing />
+      </div>
+    </>
+  );
+}
