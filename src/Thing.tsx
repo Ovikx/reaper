74b26e2bcc64b64f8ff4041f4da7582edf6ad50b
Ovikx <ovikdas10@gmail.com>
2023-07-13 11:26:29 -0400

    add config files

--- src/Thing.tsx
+++
@@ -1,5 +0,0 @@
-export function Thing() {
-    return (
-        <p className='underline text-white'>Hello world!</p>
-    );
-}
--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,9 @@
 import './index.css';
-import { Thing } from './Thing';
 
 export function App() {
+  const thing = 3;
   return (
-    <>
-      <div className="flex-1 bg-gray-900">
-        <Thing />
-      </div>
-    </>
+    <div className="bg-gray-900 min-h-screen flex justify-center"><h1 className='text-white p-10'>Hello world!</h1>
+    </div>
   );
 }
