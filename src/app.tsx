c8c0b095d50b7d0875e65ac5d6062db30323981e
Ovikx <ovikdas10@gmail.com>
2023-07-13 12:14:30 -0400

    config and linting

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
-import './index.css';
+import "./index.css";
 
 export function App() {
-  const thing = 3;
+  const t = 3;
   return (
-    <div className="bg-gray-900 min-h-screen flex justify-center"><h1 className='text-white p-10'>Hello world!</h1>
+    <div className="bg-gray-900 min-h-screen flex justify-center">
+      <h1 className="text-white p-10">Hello world!</h1>
     </div>
   );
 }
